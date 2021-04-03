411b9239d7f335750c0f90ccf7949f06ae3d8349
MilaBezaliuc <bezaliuc@gmail.com>
2021-04-03 22:02:33 +0300

    Initial commit

---
+++ src/main/java/com/cube/MedBook/MedBookApplication.java
@@ -0,0 +1,13 @@
+package com.cube.MedBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cube/MedBook/MedBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cube.MedBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
